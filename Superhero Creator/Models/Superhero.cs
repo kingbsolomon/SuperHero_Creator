142a1205bd494752fa9f9ec65557206be6fd9584
Brent Solomon <bjsolomon@gmail.com>
2020-07-14 15:03:08 -0400

    Initial upload and creation of superhero class under Models

---
+++ Superhero Creator/Models/Superhero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superhero_Creator.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AltEgo { get; set; }
+        public string PrimAbility { get; set; }
+        public string SecAbility { get; set; }
+        public string CatchPhrase { get; set; }
+    }
+}
