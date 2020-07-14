f4fd02f4ec693f8130c7522c5170c80299d24871
Brent Solomon <bjsolomon@gmail.com>
2020-07-14 17:17:37 -0400

    Updated Controller to connect to Database

---
+++ Superhero Creator/Controllers/SuperheroController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Superhero_Creator.Data;
+using Superhero_Creator.Models;
+
+namespace Superhero_Creator.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        private ApplicationDbContext _context;
+        public SuperheroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: SuperheroController
+        public ActionResult Index()
+        {
+            //query the db for all the heroes
+            //pass that into the view
+            return View();
+        }
+
+        // GET: SuperheroController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SuperheroController/Create
+        public ActionResult Create()
+        {
+            Superhero superhero = new Superhero();
+            return View(superhero);
+        }
+
+        // POST: SuperheroController/Create
+        [HttpPost]
+        public ActionResult Create(Superhero superhero)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Superhero Creator/Data/Migrations/20200714192313_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Superhero_Creator.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Superheroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    AltEgo = table.Column<string>(nullable: true),
+                    PrimAbility = table.Column<string>(nullable: true),
+                    SecAbility = table.Column<string>(nullable: true),
+                    CatchPhrase = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Superheroes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Superheroes");
+        }
+    }
+}
